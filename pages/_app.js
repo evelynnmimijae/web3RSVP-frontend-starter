b31a98fe3f1032ad7f9688fe58e5ab94418a959d
Evelynn Jones <evelynn.ajones@gmail.com>
2022-09-03 12:02:23 -0500

    Chains

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function MyApp({ Component, pageProps }) {
   return (
